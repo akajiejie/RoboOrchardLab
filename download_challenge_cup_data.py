2345f67c6ec51bf8e853a84658c7749a692f17c9
akajiejie <1035207328@qq.com>
2025-10-01 15:59:02 +0800

    update

--- download_challenge_cup_data.py
+++ download_challenge_cup_data.py
@@ -9,12 +9,9 @@ snapshot_download(
     repo_type="dataset",
     local_dir="./Real-World-Dataset/",  # 建议指定一个清晰的本地目录
     allow_patterns=[
-        "arrow_dataset_stack_bowls_three_2025_09_09/*",  # 匹配所有包含"2025_09_"的文件夹及其内部所有内容 :cite[2]
-        "lmdb_dataset_stack_bowls_three_2025_09_09/*",  # 匹配所有包含"2025_09_"的文件夹及其内部所有内容 :cite[2]
-        "urdf/*",
-        # "*.md",  # 通常包含重要的数据集说明，建议保留
-        # "*.txt",
-        # "*.json"
+        "arrow_dataset_stack_bowls_three_2025_09_09/*", 
+        "lmdb_dataset_stack_bowls_three_2025_09_09/*",  
+        "urdf/*"
     ],
     resume_download=True,  # 启用断点续传 :cite[3]
     local_dir_use_symlinks="auto"  # 让库自动决定是否使用符号链接 :cite[10]
